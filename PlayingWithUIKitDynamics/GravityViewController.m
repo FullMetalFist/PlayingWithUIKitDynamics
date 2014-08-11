2739778fb7f6a94a989d10f508bc77fb2e57aed1
Michael Vilabrera <m.vilabrera@gmail.com>
2014-08-11 10:06:21 -0400

    first detect and react collision feature

--- PlayingWithUIKitDynamics/GravityViewController.m
+++ PlayingWithUIKitDynamics/GravityViewController.m
@@ -10,9 +10,8 @@
 
 @interface GravityViewController ()
 
-@property (nonatomic, strong) UIView *squareView;
+@property (nonatomic, strong) NSMutableArray *squareViews;
 @property (nonatomic, strong) UIDynamicAnimator *animator;
-@property (nonatomic, strong) UIGravityBehavior *gravity;
 
 @end
 
@@ -22,17 +21,31 @@
 {
     [super viewDidAppear:animated];
     
-    // create the view and add it to self.view
+    NSUInteger const NumberOfViews = 2;
     
-    self.squareView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 100.0f, 100.0f)];
-    self.squareView.backgroundColor = [UIColor greenColor];
-    self.squareView.center = self.view.center;
-    [self.view addSubview:self.squareView];
+    self.squareViews = [[NSMutableArray alloc] initWithCapacity:NumberOfViews];
+    NSArray *colors = @[[UIColor redColor], [UIColor greenColor]];
+    
+    CGPoint currentCenterPoint = self.view.center;
+    CGSize eachViewSize = CGSizeMake(50.0f, 50.0f);
+    for (NSUInteger counter = 0; counter < NumberOfViews; counter++) {
+        UIView *aView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, eachViewSize.width, eachViewSize.height)];
+        aView.backgroundColor = colors[counter];
+        aView.center = currentCenterPoint;
+        
+        currentCenterPoint.y += eachViewSize.height + 10.0f;
+        
+        [self.view addSubview:aView];
+        [self.squareViews addObject:aView];
+    }
     
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    UIGravityBehavior *gravity = [[UIGravityBehavior alloc] initWithItems:self.squareViews];
+    [self.animator addBehavior:gravity];
     
-    self.gravity = [[UIGravityBehavior alloc] initWithItems:@[self.squareView]];
-    [self.animator addBehavior:self.gravity];
+    UICollisionBehavior *collision = [[UICollisionBehavior alloc] initWithItems:self.squareViews];
+    collision.translatesReferenceBoundsIntoBoundary = YES;
+    [self.animator addBehavior:collision];
 }
 
 @end
